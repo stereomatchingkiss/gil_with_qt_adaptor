3c9088a2c8a9ed9c6b5073b7a27b53ae0e39e7b2
stereomatching <luke@luke-PC.(none)>
2012-06-11 00:58:09 +0800

    comment out "read_bit_aligned_image()" since it is under development

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include "iview_factory_unit_test.hpp"
 
 int main(int argc, char *argv[])
-{        
-    //read_bit_aligned_image();
+{                
+    //read_bit_aligned_image(); //haven't finished yet
     read_packed_aligned_image();
 
     return 0;

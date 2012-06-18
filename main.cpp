5127f2178c1a80fb24457766f68e8973bf570edd
stereomatching <luke@luke-PC.(none)>
2012-06-19 03:23:28 +0800

    add one more unit test "read_packed_aligned_image_dy"

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
+#include "iview_factory.hpp"
 #include "iview_factory_unit_test.hpp"
 
 int main()
 {                
     //read_bit_aligned_image(); //haven't finished yet
-    read_packed_aligned_image();
+    //read_packed_aligned_image();
+    read_packed_aligned_image_dy();
 
     return 0;
 }

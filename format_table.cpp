69f32c0303b34088f49802780be36b7b47516dd3
stereomatching <luke@luke-PC.(none)>
2012-06-19 03:18:31 +0800

    add "_" before private data member of class "format_table"

--- format_table.cpp
+++ format_table.cpp
@@ -2,20 +2,20 @@
 
 format_table::format_table()
 {
-    bit_aligned_table.insert(std::make_pair(1, "Format_Mono"));
-    bit_aligned_table.insert(std::make_pair(2, "Format_MonoLSB"));
-    bit_aligned_table.insert(std::make_pair(8, "Format_ARGB8565_Premultiplied"));
-    bit_aligned_table.insert(std::make_pair(9, "Format_RGB666"));
-    bit_aligned_table.insert(std::make_pair(10, "Format_ARGB6666_Premultiplied"));
-    bit_aligned_table.insert(std::make_pair(12, "Format_ARGB8555_Premultiplied"));
-    bit_aligned_table.insert(std::make_pair(13, "Format_RGB888"));
+    _bit_aligned_table.insert(std::make_pair(1, "Format_Mono"));
+    _bit_aligned_table.insert(std::make_pair(2, "Format_MonoLSB"));
+    _bit_aligned_table.insert(std::make_pair(8, "Format_ARGB8565_Premultiplied"));
+    _bit_aligned_table.insert(std::make_pair(9, "Format_RGB666"));
+    _bit_aligned_table.insert(std::make_pair(10, "Format_ARGB6666_Premultiplied"));
+    _bit_aligned_table.insert(std::make_pair(12, "Format_ARGB8555_Premultiplied"));
+    _bit_aligned_table.insert(std::make_pair(13, "Format_RGB888"));
 
-    packed_aligned_table.insert(std::make_pair(3, "Format_Indexed8")); //gray8_view_t
-    packed_aligned_table.insert(std::make_pair(4, "Format_RGB32")); //bgra8_view_t
-    packed_aligned_table.insert(std::make_pair(5, "Format_ARGB32")); //bgra8_view_t
-    packed_aligned_table.insert(std::make_pair(6, "Format_ARGB32_Premultiplied")); //bgra8_view_t
-    packed_aligned_table.insert(std::make_pair(7, "Format_RGB16")); //rgb565_view_t
-    packed_aligned_table.insert(std::make_pair(11, "Format_RGB555")); //rgb655_view_t
-    packed_aligned_table.insert(std::make_pair(14, "Format_RGB444")); //bgra4444_view_t
-    packed_aligned_table.insert(std::make_pair(15, "Format_ARGB4444_Premultiplied")); //bgra4444_view_t
+    _packed_aligned_table.insert(std::make_pair(3, "Format_Indexed8")); //gray8_view_t
+    _packed_aligned_table.insert(std::make_pair(4, "Format_RGB32")); //bgra8_view_t
+    _packed_aligned_table.insert(std::make_pair(5, "Format_ARGB32")); //bgra8_view_t
+    _packed_aligned_table.insert(std::make_pair(6, "Format_ARGB32_Premultiplied")); //bgra8_view_t
+    _packed_aligned_table.insert(std::make_pair(7, "Format_RGB16")); //rgb565_view_t
+    _packed_aligned_table.insert(std::make_pair(11, "Format_RGB555")); //rgb655_view_t
+    _packed_aligned_table.insert(std::make_pair(14, "Format_RGB444")); //bgra4444_view_t
+    _packed_aligned_table.insert(std::make_pair(15, "Format_ARGB4444_Premultiplied")); //bgra4444_view_t
 }
--- format_table.hpp
+++ format_table.hpp
@@ -28,18 +28,18 @@ class format_table
 {
   public :
     format_table();
-    std::map<int, QString> &get_bit_aligned_table() { return bit_aligned_table; }
-    std::map<int, QString> &get_packed_aligned_table() { return packed_aligned_table; }
-    std::map<int, QString> const &get_bit_aligned_table() const { return bit_aligned_table; }
-    std::map<int, QString> const &get_packed_aligned_table() const { return packed_aligned_table; }
+    std::map<int, QString> &get_bit_aligned_table() { return _bit_aligned_table; }
+    std::map<int, QString> &get_packed_aligned_table() { return _packed_aligned_table; }
+    std::map<int, QString> const &get_bit_aligned_table() const { return _bit_aligned_table; }
+    std::map<int, QString> const &get_packed_aligned_table() const { return _packed_aligned_table; }
 
   private :
     format_table(format_table const&);
     format_table operator=(format_table const&);
 
   private :
-    std::map<int, QString> bit_aligned_table;
-    std::map<int, QString> packed_aligned_table;
+    std::map<int, QString> _bit_aligned_table;
+    std::map<int, QString> _packed_aligned_table;
 };
 
 #endif // FORMAT_TABLE_HPP
